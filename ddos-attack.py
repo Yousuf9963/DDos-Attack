128fefccc1912cc785e9b874238e7c27eacda8bf
Yousuf9963 <113084773+Yousuf9963@users.noreply.github.com>
2022-12-23 16:21:07 +0300

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -64,23 +64,23 @@ os.system("clear")
 
 os.system("figlet Attack Starting")
 
-print "[                    ] 0% "
+print"[                    ] 0% "
 
 time.sleep(5)
 
-print "[=====               ] 25%"
+print"[=====               ] 25%"
 
 time.sleep(5)
 
-print "[==========          ] 50%"
+print"[==========          ] 50%"
 
 time.sleep(5)
 
-print "[===============     ] 75%"
+print"[===============     ] 75%"
 
 time.sleep(5)
 
-print "[====================] 100%"
+print"[====================] 100%"
 
 time.sleep(3)
 
