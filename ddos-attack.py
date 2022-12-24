c30174f04697219f87d40dbd4b2798c604dc51f8
Yousuf9963 <113084773+Yousuf9963@users.noreply.github.com>
2022-12-24 03:37:57 +0300

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -52,15 +52,15 @@ print "[+] Version:1.0"
 
 print "[+] Team:Junior Programmers."
 
-print "[+] Github:https://github.com/Yousuf9963/phone-num-info."
+print "[+] Github: https://github.com/Yousuf9963/phone-num-info."
 
 print "[+] Follow me on Github: https://github.com/Yousuf9963."
 
-print "[-] Website muhammadabdirahman.wixsite.com/yousuf9963blog."
+print "[-] Website: muhammadabdirahman.wixsite.com/yousuf9963blog."
 
 print "[!] legal disclaimer: Usage of this Program for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program."
 
-print "[+] I hope for you good future and i am willing that you will come high effort."
+print "Motto: [+] I hope for you good future and i am willing that you will come high effort."
 
 print     
 
